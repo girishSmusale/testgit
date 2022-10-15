9e416221f9e58fa6d654f897d2d804ba29c58483
admin <admin@DESKTOP-RPHHSS0>
2022-10-15 23:52:53 +0530

    my first commit

---
+++ mytestgit/src/com/mytestgit/Main.java
@@ -0,0 +1,10 @@
+ package com.mytestgit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is git hub test  2 ");
+	}
+
+}
